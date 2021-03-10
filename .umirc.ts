65ee34d424a0b17d260c519576a7198d166666c2
upup-ubuntu <2213311288@qq.com>
2021-03-10 20:01:05 +0800

    确定应用基本结构

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,17 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  title: "Q",
   routes: [
+    {
+      path: '/',
+      component: '@/layouts/BasicLayout.tsx',
+      routes:[
+        { path: '/q/post', component: '@/pages/Question/PostQuestionPage.jsx' },
+        { path: '/q/fish', component: '@/pages/Question/FishQuestionPage.jsx' },
+        { path: '/q/my', component: '@/pages/Question/MyQuestionPage.jsx' ,title:'我的Q'},
+      ]
+    },
   ],
   fastRefresh: {},
 });
